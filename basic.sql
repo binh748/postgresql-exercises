74a9a6f5c4d62a8c260db3170664258e983cbf8c
binh748 <binh.hoang7@gmail.com>
2020-11-21 18:22:49 -0500

    Complete first two basic exercises

---
+++ basic.sql
@@ -0,0 +1,13 @@
+/* Contains my answers to the "Basic" exercises on pgexercises.com */
+
+-- 1. Retrieve everything from a table
+
+SELECT *
+  FROM cd.facilities;
+
+-- 2. Retrieve specific columns from a table
+
+SELECT name, membercost
+  FROM cd.facilities;
+
+  
