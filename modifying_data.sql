00b25c4d47e240f5ca65b2d4783a2393bab0eb46
binh748 <binh.hoang7@gmail.com>
2020-11-27 14:11:10 -0500

    Finish modifying data exercises

--- modifying_data.sql
+++ modifying_data.sql
@@ -22,8 +22,8 @@ VALUES
 INSERT INTO
 	cd.facilities(facid, name, membercost, guestcost, initialoutlay, monthlymaintenance)
 VALUES
-	((SELECT MAX(facid)+1
-	  FROM cd.facilities), 'Spa', 20, 30, 100000, 800);
+	((SELECT MAX(facid)
+	    FROM cd.facilities)+1, 'Spa', 20, 30, 100000, 800);
 
 -- 4. Update some existing data
 
@@ -31,7 +31,7 @@ UPDATE cd.facilities
    SET initialoutlay = 10000
  WHERE name = 'Tennis Court 2';
 
- -- 5. Update multiple rows and columns at the same time
+-- 5. Update multiple rows and columns at the same time
 
 -- When using a CASE clause to update rows, I always need to supply an ELSE condition
 -- to tell SQL how to populate the column for when the condition is not met.
@@ -44,3 +44,41 @@ UPDATE cd.facilities
    SET membercost = 6,
        guestcost = 30
  WHERE name LIKE 'Tennis Court%';
+
+-- 6. Update a row based on the contents of another row
+
+UPDATE cd.facilities
+   SET membercost = (SELECT membercost
+					             FROM cd.facilities
+					            WHERE name = 'Tennis Court 1') * 1.1,
+	     guestcost = (SELECT guestcost
+					            FROM cd.facilities
+					           WHERE name = 'Tennis Court 1') * 1.1
+ WHERE name = 'Tennis Court 2';
+
+-- 7. Delete all bookings
+
+TRUNCATE cd.bookings;
+
+DELETE FROM cd.bookings;
+
+-- 8. Delete a member from the cd.members table
+
+DELETE FROM cd.members
+ WHERE memid = 37;
+
+-- 9. Delete based on subquery
+
+DELETE FROM cd.members
+ WHERE memid NOT IN (SELECT memid
+					             FROM cd.bookings);
+
+-- The below is a correlated subquery that reads like this:
+-- For every row in cd.members, check if that row has a memid
+-- that is in cd.bookings; if not, then delete that row.
+DELETE FROM cd.members AS m
+ WHERE NOT EXISTS
+       (SELECT 1
+          FROM cd.bookings
+         WHERE memid = m.memid);
+  
